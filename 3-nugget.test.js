afe7cdb599f6ebaef36d19e02fb47670229dd78d
Blossom <saphashb@gmail.com>
2022-09-06 15:26:14 +0100

    difference between ! and !!

--- 3-nugget.test.js
+++ 3-nugget.test.js
@@ -1,1 +1,11 @@
 // difference between ! and !!
+
+// ! the logical NOT operator returns the inverse boolean of the value given to it.
+// falsy values becomes truthy and vice-versa
+
+// !! the double not operator returns the boolean of the value given to it
+
+
+
+console.log(!0)
+console.log(!!0)
