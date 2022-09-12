fd53539e16ee2a76a3604aadfb1475f1bb6802bd
Blossom <saphashb@gmail.com>
2022-09-12 14:24:27 +0100

    #5 bits and pieces

---
+++ 5-bap.test.js
@@ -0,0 +1,40 @@
+// conditional in map function
+
+const students = [
+  {
+    name: "Blossom",
+    age: "16",
+    location: "Lagos",
+    hobbies: ['reading, exercise']
+  },
+  {
+    name: "Liza",
+    age: "18",
+    location: "Paris",
+    hobbies: ['horse-racing, exercise']
+  },
+  {
+    name: "Dani",
+    age: "14",
+    location: "Lagos",
+    hobbies: ['fishing, football']
+  }
+]
+
+students.map(item => {
+  if (item.location == "Lagos") {
+    console.log(item)
+  }
+})
+
+students.map(item => {
+  item.hobbies.toString().includes('exercise') &&
+    console.log(item)
+
+})
+
+students.map(item => {
+  item.hobbies.toString().includes('exercise') ?
+    console.log(item) : ""
+
+})
