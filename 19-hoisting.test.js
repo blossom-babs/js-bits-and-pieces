a25e9e0f0e9c8f815e9f32eaf06f34dd418dcaed
Blossom <saphashb@gmail.com>
2022-10-17 12:14:45 +0100

    rename files

---
+++ 19-hoisting.test.js
@@ -0,0 +1,56 @@
+// Hoisting in Javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Javascript hoisting refers to the 
+process whereby the JS interpreter moves
+the declaration of functions, variables 
+or classes to the top of their scope
+prior to the execution
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function hoisting
+
+greeting()
+
+function greeting(){
+  console.log('hello world')
+}
